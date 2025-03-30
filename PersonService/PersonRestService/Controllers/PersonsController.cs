1c9e3bf0712b9be4288ba1c8e9ee830e133d099c
MatiasHolmNielsen <10519141@ucn.dk>
2025-03-31 00:39:01 +0200

    add

---
+++ PersonService/PersonRestService/Controllers/PersonsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonRestService.BusinesslogicLayer;
+using PersonRestService.DTOs;
+
+namespace PersonRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonData _businessLogicCtrl;
+
+        public PersonsController(IPersonData inBusinessLogicCtrl)
+        {
+            _businessLogicCtrl = inBusinessLogicCtrl;
+        }
+
+        // GET: api/persons
+        [HttpGet]
+        public ActionResult<List<PersonDTO>> Get()
+        {
+            var persons = _businessLogicCtrl.Get();
+            if (persons == null)
+                return StatusCode(500); // Internal server error
+            if (persons.Count == 0)
+                return NoContent(); // 204
+            return Ok(persons); // 200
+        }
+
+        // GET: api/persons/3
+        [HttpGet("{id}")]
+        public ActionResult<PersonDTO> Get(int id)
+        {
+            var person = _businessLogicCtrl.Get(id);
+            if (person == null)
+                return NotFound(); // 404
+            return Ok(person); // 200
+        }
+
+        // POST: api/persons
+        [HttpPost]
+        public ActionResult<int> PostNewPerson([FromBody] PersonDTO inPersonDto)
+        {
+            if (inPersonDto == null) return BadRequest();
+
+            int insertedId = _businessLogicCtrl.Add(inPersonDto);
+            if (insertedId > 0)
+                return Ok(insertedId);
+            if (insertedId == 0)
+                return BadRequest(); // Missing input
+            return StatusCode(500); // Error
+        }
+    }
+}
--- PersonService/PersonRestService/Program.cs
+++ PersonService/PersonRestService/Program.cs
@@ -1,36 +1,28 @@
+using PersonData.DatabaseLayer;
+using PersonRestService.BusinesslogicLayer;
 
-namespace PersonRestService
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register services for DI
+builder.Services.AddSingleton<IPersonAccess, PersonDatabaseAccess>();
+builder.Services.AddSingleton<IPersonData, PersonDataLogic>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
