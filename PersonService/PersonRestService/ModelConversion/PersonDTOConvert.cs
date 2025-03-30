db88439b46a85ad2e24da3bbaea7f1ff5e894fa1
MatiasHolmNielsen <10519141@ucn.dk>
2025-03-31 00:26:56 +0200

    added

---
+++ PersonService/PersonRestService/ModelConversion/PersonDTOConvert.cs
@@ -0,0 +1,36 @@
+﻿using PersonData.ModelLayer;
+using PersonRestService.DTOs;
+
+namespace PersonRestService.ModelConversion
+{
+    public class PersonDTOConvert
+    {
+        public static List<PersonDTO?>? FromPersonCollection(List<Person> inPersons)
+        {
+            if (inPersons == null) return null;
+
+            List<PersonDTO?> list = new();
+            foreach (var person in inPersons)
+            {
+                if (person != null)
+                    list.Add(FromPerson(person));
+            }
+
+            return list;
+        }
+
+        public static PersonDTO? FromPerson(Person person)
+        {
+            return person != null
+                ? new PersonDTO(person.FirstName, person.LastName, person.MobilePhone)
+                : null;
+        }
+
+        public static Person? ToPerson(PersonDTO dto)
+        {
+            return dto != null
+                ? new Person(dto.FirstName, dto.LastName, dto.MobilePhone)
+                : null;
+        }
+    }
+}
