22f2ad54647535197d38cbc46f2cbb110e576fc1
MatiasHolmNielsen <10519141@ucn.dk>
2025-03-31 00:18:25 +0200

    new

--- PersonService/PersonRestService/DTOs/PersonDTO.cs
+++ PersonService/PersonRestService/DTOs/PersonDTO.cs
@@ -2,5 +2,19 @@
 {
     public class PersonDTO
     {
+        public PersonDTO() { }
+
+        public PersonDTO(string? firstName, string? lastName, string? mobilePhone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MobilePhone = mobilePhone;
+        }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MobilePhone { get; set; }
+
+        public string? FullName => $"{FirstName} {LastName}";
     }
 }
