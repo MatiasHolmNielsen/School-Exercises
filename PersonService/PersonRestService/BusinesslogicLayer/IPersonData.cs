12e63526a97fd9be358934d32e2eeb7c3ec8e880
MatiasHolmNielsen <10519141@ucn.dk>
2025-03-31 00:30:16 +0200

    Added

---
+++ PersonService/PersonRestService/BusinesslogicLayer/IPersonData.cs
@@ -0,0 +1,14 @@
+﻿using PersonRestService.DTOs;
+using System.Collections.Generic;
+
+namespace PersonRestService.BusinesslogicLayer
+{
+    public interface IPersonData
+    {
+        PersonDTO? Get(int id);
+        List<PersonDTO?>? Get();
+        int Add(PersonDTO personToAdd);
+        bool Put(PersonDTO personToUpdate);
+        bool Delete(int id);
+    }
+}
---
+++ PersonService/PersonRestService/BusinesslogicLayer/PersonDataLogic.cs
@@ -0,0 +1,62 @@
+﻿using PersonData.DatabaseLayer;
+using PersonData.ModelLayer;
+using PersonRestService.DTOs;
+using PersonRestService.ModelConversion;
+using System;
+using System.Collections.Generic;
+
+namespace PersonRestService.BusinesslogicLayer
+{
+    public class PersonDataLogic : IPersonData
+    {
+        private readonly IPersonAccess _personAccess;
+
+        public PersonDataLogic(IPersonAccess personAccess)
+        {
+            _personAccess = personAccess;
+        }
+
+        public int Add(PersonDTO personToAdd)
+        {
+            try
+            {
+                var person = PersonDTOConvert.ToPerson(personToAdd);
+                return person != null ? _personAccess.CreatePerson(person) : 0;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public bool Delete(int id) => throw new NotImplementedException();
+
+        public PersonDTO? Get(int id)
+        {
+            try
+            {
+                var person = _personAccess.GetPersonById(id);
+                return PersonDTOConvert.FromPerson(person);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<PersonDTO?>? Get()
+        {
+            try
+            {
+                var persons = _personAccess.GetPersonAll();
+                return PersonDTOConvert.FromPersonCollection(persons);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Put(PersonDTO personToUpdate) => throw new NotImplementedException();
+    }
+}
